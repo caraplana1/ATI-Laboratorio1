9f2d4a828f91c6f397c55927fabc3d4214c17baf
caraplana1 <cristianlandaez@gmail.com>
2025-05-04 19:09:50 -0400

    configuración desde javascript

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let configEs
+
+document.addEventListener('DOMContentLoaded', () => {
+	fetch('../conf/configES.json')
+	.then(respuesta => {
+		if (!respuesta.ok) throw new Error('Error al cargar la configuración');
+		return respuesta.json();
+	})
+	.then(config => {
+		cargarConfig(config);
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+});
+
+function cargarConfig(config){
+	document.getElementById("titulo-index").textContent = config.sitio[0] + " " + config.sitio[1] + " " + config.sitio[2]
+	document.getElementById("saludo").textContent = config.saludo + ", "
+	document.getElementById("footer").innerText = config.copyRight
+	document.getElementById("button").innerText = config.buscar
+	let placeholder = document.getElementsByName("Nombre")
+	placeholder[0].placeholder = " " + config.nombre
+}
