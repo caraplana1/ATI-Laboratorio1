f6dbcf034d59505447b09ae850156ae420e71717
caraplana1 <cristianlandaez@gmail.com>
2025-05-07 22:30:51 -0400

    Perfil carga desde json

--- js/perfil.js
+++ js/perfil.js
@@ -16,8 +16,21 @@ function CargarJsonConfig() {
 	});
 }
 
-function cargarConfig(config)
-{
+function CargarJsonAlumni() {
+	fetch('./../27597219/perfil.json')
+	.then(respuesta => {
+		if (!respuesta.ok) throw new Error('Error al cargar la configuración');
+		return respuesta.json();
+	})
+	.then(datos => {
+		CargarAlumno(datos);
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+}
+
+function cargarConfig(config) {
 	let preguntas = document.getElementsByClassName("pregunta")
 	preguntas[0].textContent = config.color + ":"
 	preguntas[1].textContent = config.musica + ":"
@@ -29,7 +42,21 @@ function cargarConfig(config)
 
 }
 
+function CargarAlumno(datos) {
+	document.getElementById("titulo-tarjeta").textContent = datos.nombre
+	document.getElementById("description").textContent = datos.descripcion
+	respuestas = document.getElementsByClassName("derecha")
+	respuestas[0].textContent = datos.color
+	respuestas[1].textContent = datos.musica
+	respuestas[2].textContent = datos.libro
+	respuestas[3].textContent = datos.video_juego
+	respuestas[4].textContent = datos.lenguajes
+	let email = document.getElementsByClassName("email")[0]
+	email.href= "mailto:" + datos.email
+	email.textContent = datos.email
+}
+
 function Main(){
 	CargarJsonConfig();
-
+	CargarJsonAlumni();
 }
